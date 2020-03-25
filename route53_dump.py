70f49369d7efdbd03f757eb05823c7ed7e65c6b2
Andres Riancho <andres.riancho@wildlifestudios.com>
2020-03-25 14:29:07 -0300

    Add script to dump route53 domains

---
+++ route53_dump.py
@@ -0,0 +1,93 @@
+"""
+Dump all route53 records
+"""
+
+from utils.json_printer import json_printer
+from utils.json_writer import json_writer
+from utils.session import get_session
+
+PRINT_NAMES = ('A', 'CNAME')
+
+
+def print_interesting(record):
+    record_name = record.get('Name', None)
+
+    if record.get('Type') not in PRINT_NAMES:
+        return
+
+    # remove trailing dot
+    record_name = record_name[:-1]
+
+    if record_name.endswith('internal'):
+        return
+
+    # replace the * at the beginning of the record name
+    record_name = record_name.replace('*', 'www')
+
+    print(record_name)
+
+
+def dump_route53_records(route53_client, zone_name, zone_id, all_data, startrrname=''):
+    if startrrname:
+        response = route53_client.list_resource_record_sets(
+            HostedZoneId=zone_id,
+            StartRecordName=startrrname
+        )
+    else:
+        response = route53_client.list_resource_record_sets(
+            HostedZoneId=zone_id
+        )
+
+    for record in response['ResourceRecordSets']:
+        record_name = record.get('Name', None)
+
+        if record_name is None:
+            continue
+
+        if record_name in all_data:
+            continue
+
+        record_name = record_name.replace('\\052', '*')
+        record['Name'] = record_name
+
+        if zone_name not in all_data:
+            all_data[zone_name] = list()
+            
+        all_data[zone_name].append(record)
+
+        print_interesting(record)
+
+    if 'NextRecordName' not in response['ResourceRecordSets']:
+        return all_data
+
+    dump_route53_records(
+        route53_client,
+        zone_name,
+        zone_id,
+        all_data,
+        startrrname=response['ResourceRecordSets']['NextRecordName'])
+
+    return all_data
+
+
+def main():
+    session = get_session()
+    route53_client = session.client('route53')
+
+    zone_info = route53_client.list_hosted_zones()
+
+    all_data = dict()
+
+    for zone in zone_info.get('HostedZones'):
+        zone_name = zone['Name']
+        zone_id = zone['Id']
+
+        dump_route53_records(route53_client, zone_name, zone_id, all_data)
+
+    json_writer('output/route53_dump.json', all_data)
+    return all_data
+
+
+if __name__ == '__main__':
+    all_data = main()
+    # json_printer(all_data)
