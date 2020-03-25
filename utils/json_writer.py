8acd0e1cf53cd12601e3ead9aa32ee4dcd59cdb5
Andres Riancho <andres.riancho@wildlifestudios.com>
2020-03-25 14:28:54 -0300

    Create output directory

--- utils/json_writer.py
+++ utils/json_writer.py
@@ -1,9 +1,12 @@
 import json
+import os
 
 from utils.json_encoder import json_encoder
 
 
 def json_writer(filename, data):
+    os.makedirs('output', exist_ok=True)
+
     data_str = json.dumps(data,
                           indent=4,
                           sort_keys=True,
